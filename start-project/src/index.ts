4969656d4dc6651be600166c22be04112b4f1553
Erlan Ibraev <tyumenerlan.i@gmail.com>
2024-05-30 14:03:49 +0500

    Patch

--- start-project/src/index.ts
+++ start-project/src/index.ts
@@ -4,7 +4,7 @@ import { addressesRouter } from "./routes/addresses-router";
 
 
 const app = express()
-const port = 3000
+const port = 5000
 
 const parserMiddleware = express.json()
 app.use(parserMiddleware)
